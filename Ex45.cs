0af7487f2565c0c9fcfecafe25a7cfdbd32e52f6
unknown <A0123058M@CR3549.iss.nus.edu.sg>
2014-08-20 17:47:50 +0800

    ex 45

---
+++ Ex45.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace day1
+{
+    class Ex45
+    {
+        static void Main(string[] args)
+        {
+            double number = 0.25;
+            double numberOfCents=0.0
+            number =number* Math.Pow(10, 2);
+            Console.WriteLine(number);
+            int r = (int)number % 100;
+            int q = (int)number / 100;
+            Console.WriteLine(q);
+            Console.WriteLine(r);
+            if (q > 0)
+            {
+            }
+        }
+    }
+}
